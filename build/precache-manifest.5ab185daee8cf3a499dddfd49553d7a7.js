c006d19e576b42d0a00169c62584b853bad4f6dd
Geetanshu <chaudharygeetanshu719@gmail.com>
2024-07-07 14:19:53 +0530

    Initial commit

---
+++ build/precache-manifest.5ab185daee8cf3a499dddfd49553d7a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c36d86c58abd3854ae747fd0ce82ff",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "dc0afccf5871d2cbcfcc",
+    "url": "/Keeper/static/css/main.110234db.chunk.css"
+  },
+  {
+    "revision": "bc3abac1475c0bf3691f",
+    "url": "/Keeper/static/js/2.288a3609.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/Keeper/static/js/2.288a3609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0afccf5871d2cbcfcc",
+    "url": "/Keeper/static/js/main.0c908dca.chunk.js"
+  },
+  {
+    "revision": "d6003daab4c42b364391",
+    "url": "/Keeper/static/js/runtime-main.da77df16.js"
+  }
+]);
